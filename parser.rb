18be1d2318e097891b537638cf88d381894e61ed
Jason Hite <hite77@gmail.com>
2016-06-02 21:53:22 -0400

    Parses folders

--- parser.rb
+++ parser.rb
@@ -6,13 +6,11 @@ class Parser
 
   def parse(html_data)
     counter = 0
+    baseFolderName="https://192.168.50.146/nowplaying/"
     html_data.gsub! '&quot;', '"'
+    html_data.gsub! '&amp;', '&'
     while true do
       line = findPieceOfString(html_data,"<tr ","</tr>")
-      html_data = html_data[html_data.index("</tr>")+5..-1]
-      if html_data.index("</tr>").nil?
-        break;
-      end
       folder = findPieceOfString(line,"<a href=\"","\">folder</a>")
       if folder.nil?
         title = findPieceOfString(line,"<b>","</b>")
@@ -21,11 +19,20 @@ class Parser
           @h[counter]["title"] = title
           @h[counter]["description"] = findPieceOfString(line,"<br>","</td>")
           counter = counter + 1
+        end # if !title.nil
+      else # if folder.nil
+        if @h["folders"].nil?
+          @h["folders"] = Array.new
         end
+        @h["folders"] << baseFolderName+folder
       end
-    end
+      html_data = html_data[html_data.index("</tr>")+5..-1]
+      if html_data.index("</tr>").nil?
+        break;
+      end
+    end # while true do
     @h
-  end
+  end # parse method
   
   def findPieceOfString(string,b,e)
     if (string.index(b).nil? || string.index(e).nil?)
